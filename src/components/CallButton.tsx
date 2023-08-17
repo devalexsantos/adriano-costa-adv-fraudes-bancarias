5c3d79acaa6bd2624cc0e670e44b28817a481771
Alex Santos <devalexsantos@gmail.com>
2023-08-17 08:16:44 -0300

    fixing call number button

--- src/components/CallButton.tsx
+++ src/components/CallButton.tsx
@@ -1,6 +1,6 @@
 export default function CallButton() {
   return (
-    <a href="tel:+5571999999999">
+    <a href="tel:+5571991656882">
       <div className="sm:hidden fixed bottom-10 left-6 rounded-full bg-cyan-600 p-4 shadow-xl">
         <img src="./assets/call-icon.svg" alt="Ícone de um telefone" />
       </div>
